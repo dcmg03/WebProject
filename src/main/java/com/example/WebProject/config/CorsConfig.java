66873be47c8f21310ba7d145a52a3fa46f89b52d
DeisyMonroy <deisy.monroy@uptc.edu.co>
2024-12-09 17:37:40 -0500

    presupuestos listandose correctamente

---
+++ src/main/java/com/example/WebProject/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.WebProject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Aplica a todas las rutas
+                        .allowedOrigins("http://localhost:3000") // Origen del frontend
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permitir todos los headers
+                        .allowCredentials(true); // Permitir el envío de cookies o credenciales
+            }
+        };
+    }
+}
--- src/main/java/com/example/WebProject/entityes/Rubro.java
+++ src/main/java/com/example/WebProject/entityes/Rubro.java
@@ -7,6 +7,46 @@ import java.util.List;
 @Entity
 public class Rubro {
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPresupuestoInicial() {
+        return presupuestoInicial;
+    }
+
+    public void setPresupuestoInicial(Double presupuestoInicial) {
+        this.presupuestoInicial = presupuestoInicial;
+    }
+
+    public Double getPresupuestoDisponible() {
+        return presupuestoDisponible;
+    }
+
+    public void setPresupuestoDisponible(Double presupuestoDisponible) {
+        this.presupuestoDisponible = presupuestoDisponible;
+    }
+
+    public List<Asignacion> getAsignaciones() {
+        return asignaciones;
+    }
+
+    public void setAsignaciones(List<Asignacion> asignaciones) {
+        this.asignaciones = asignaciones;
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
